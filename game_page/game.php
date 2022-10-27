b7b085c3ac7b89bd2f37d24fb9550a3c2b6754d4
NPlanche <89564236+NPlanche@users.noreply.github.com>
2022-10-27 16:12:55 -0400

    Created the Game Page

---
+++ game_page/game.php
@@ -0,0 +1,18 @@
+<?php include("../partials/navbar.php"); ?>
+
+    <section class = "game">
+          <!-- 21:9 aspect ratio 
+          <div class="embed-responsive embed-responsive-21by9">
+            <iframe class="embed-responsive-item" src="gcode/gcode.html"></iframe>
+          </div>-->
+
+
+          <p  align="center"><iframe frameBorder= "0" class= "game-iframe" src="gcode/gcode.html" title="Quantum Owl" 
+            style="overflow:auto;">
+          </iframe></p>
+    </section>
+
+
+  </body>
+
+<?php include("../partials/footer.php");?>
---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+    <footer>
+        <p class = "footer" >Copyright &copy; 2022 QuantumOwl.com</p>
+    </footer>
+</html>
